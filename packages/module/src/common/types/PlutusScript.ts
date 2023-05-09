8ca8ceec805ed17f01f6f67421044151c67b32b7
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2023-05-09 11:21:48 +0100

    implement plutusscript converters

--- packages/module/src/common/types/PlutusScript.ts
+++ packages/module/src/common/types/PlutusScript.ts
@@ -1,9 +1,31 @@
-import { LANGUAGE_VERSIONS } from '../constants';
+import { Script, ScriptType } from '@harmoniclabs/plu-ts';
+
+export const ALL_PLUTUS_VERSIONS = {
+  V1: ScriptType.PlutusV1,
+  V2: ScriptType.PlutusV2,
+};
 
 export type PlutusScript = {
-  version: LanguageVersion;
+  version: PlutusVersion;
   code: string;
 };
 
-export type LanguageVersion =
-  keyof typeof LANGUAGE_VERSIONS;
+export type PlutusVersion = keyof typeof ALL_PLUTUS_VERSIONS;
+
+export const fromScript = (script: Script): PlutusScript => {
+  const version = Object.keys(ALL_PLUTUS_VERSIONS)
+    .find((key) => ALL_PLUTUS_VERSIONS[key] === script.type);
+
+  if (version) {
+    return {
+      version: version as PlutusVersion,
+      code: script.cbor.toString(),
+    };
+  }
+
+  throw new Error('Invalid Plutus Script');
+};
+
+export const toScript = (plutusScript: PlutusScript): Script => {
+  return new Script(ALL_PLUTUS_VERSIONS[plutusScript.version], Buffer.from(plutusScript.code, 'hex'));
+};
