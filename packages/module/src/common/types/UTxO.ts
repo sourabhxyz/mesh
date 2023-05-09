e54fd396f73d6dd369b0ec0f9b7cd22f848024fd
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2023-05-09 10:15:28 +0100

    implement utxo converters

--- packages/module/src/common/types/UTxO.ts
+++ packages/module/src/common/types/UTxO.ts
@@ -1,4 +1,8 @@
-import { Asset } from './Asset';
+import {
+  Address, Hash32, Script, UTxO as TxUnspentOutput,
+  dataFromCbor, dataToCbor, hashData, isData,
+} from '@harmoniclabs/plu-ts';
+import { Asset, fromValue, toValue } from './Asset';
 
 export type UTxO = {
   input: {
@@ -12,4 +16,56 @@ export type UTxO = {
     plutusData?: string;
     scriptRef?: string;
   };
+};
+
+export const fromTxUnspentOutput = (txUnspentOutput: TxUnspentOutput): UTxO => {
+  const plutusData = isData(txUnspentOutput.resolved.datum)
+    ? dataToCbor(txUnspentOutput.resolved.datum).toString()
+    : undefined;
+
+  const dataHash = isData(txUnspentOutput.resolved.datum)
+    ? hashData(txUnspentOutput.resolved.datum).toString()
+    : txUnspentOutput.resolved.datum?.toString();
+
+  const scriptRef = txUnspentOutput.resolved.refScript
+    ?.toCbor().toString();
+
+  return <UTxO>{
+    input: {
+      outputIndex: txUnspentOutput.utxoRef.index,
+      txHash: txUnspentOutput.utxoRef.id.toString(),
+    },
+    output: {
+      address: txUnspentOutput.resolved.address.toString(),
+      amount: fromValue(txUnspentOutput.resolved.value),
+      dataHash, plutusData, scriptRef,
+    },
+  };
+};
+
+export const toTxUnspentOutput = (utxo: UTxO): TxUnspentOutput => {
+  const plutusData = utxo.output.plutusData
+    ? dataFromCbor(utxo.output.plutusData)
+    : undefined;
+
+  const dataHash = utxo.output.dataHash
+    ? new Hash32(utxo.output.dataHash)
+    : undefined;
+
+  const scriptRef = utxo.output.scriptRef
+    ? Script.fromCbor(utxo.output.scriptRef)
+    : undefined;
+
+  return new TxUnspentOutput({
+    utxoRef: {
+      id: new Hash32(utxo.input.txHash),
+      index: utxo.input.outputIndex,
+    },
+    resolved: {
+      address: Address.fromString(utxo.output.address),
+      value: toValue(utxo.output.amount),
+      datum: plutusData ?? dataHash,
+      refScript: scriptRef,
+    },
+  });
 };
