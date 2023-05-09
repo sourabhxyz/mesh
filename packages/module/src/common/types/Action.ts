e7db0fa3330aa4bed420346a6d846cbd9ef75c7b
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2023-05-09 10:14:42 +0100

    implement Action converters

--- packages/module/src/common/types/Action.ts
+++ packages/module/src/common/types/Action.ts
@@ -1,5 +1,14 @@
-import { REDEEMER_TAGS } from '@mesh/common/constants';
-import { Data } from './Data';
+import {
+  ExBudget, TxRedeemer, TxRedeemerTag,
+} from '@harmoniclabs/plu-ts';
+import { Data, toPlutusData } from './Data';
+
+const REDEEMER_TAGS = {
+  CERT: TxRedeemerTag.Cert,
+  MINT: TxRedeemerTag.Mint,
+  REWARD: TxRedeemerTag.Withdraw,
+  SPEND: TxRedeemerTag.Spend,
+};
 
 export type Action = {
   data: Data;
@@ -11,4 +20,18 @@ export type Action = {
 export type Budget = {
   mem: number;
   steps: number;
+};
+
+export const toTxRedeemer = (action: Action): TxRedeemer => {
+  const lookupRedeemerTag = (key: string): TxRedeemerTag => REDEEMER_TAGS[key];
+
+  return new TxRedeemer({
+    index: action.index,
+    tag: lookupRedeemerTag(action.tag),
+    data: toPlutusData(action.data),
+    execUnits: new ExBudget({
+      steps: action.budget.steps,
+      mem: action.budget.mem,
+    }),
+  });
 };
