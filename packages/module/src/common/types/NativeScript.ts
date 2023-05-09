27b1a2cb75bcd12f21086a5f38b45aea480752a1
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2023-05-09 11:21:13 +0100

    implement nativescript conveters

--- packages/module/src/common/types/NativeScript.ts
+++ packages/module/src/common/types/NativeScript.ts
@@ -1,18 +1,7 @@
-export type NativeScript =
-  | {
-      type: 'after' | 'before';
-      slot: string;
-    }
-  | {
-      type: 'all' | 'any';
-      scripts: NativeScript[];
-    }
-  | {
-      type: 'atLeast';
-      required: number;
-      scripts: NativeScript[];
-    }
-  | {
-      type: 'sig';
-      keyHash: string;
-    };
+import { CborString, NativeScript, nativeScriptFromCbor, nativeScriptToCbor } from '@harmoniclabs/plu-ts';
+
+export { NativeScript } from '@harmoniclabs/plu-ts';
+
+export const fromNativeScriptHex = (nativeScriptHex: string): NativeScript => nativeScriptFromCbor(new CborString(nativeScriptHex));
+
+export const toNativeScriptHex = (nativeScript: NativeScript): string => nativeScriptToCbor(nativeScript).toString();
