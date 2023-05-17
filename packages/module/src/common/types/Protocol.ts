3909f85169951767ac0e9631bbd7a4e5db974763
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2023-05-17 10:08:41 +0100

    implement toProtocolParameters

--- packages/module/src/common/types/Protocol.ts
+++ packages/module/src/common/types/Protocol.ts
@@ -1,3 +1,5 @@
+import { ProtocolParamters } from '@harmoniclabs/plu-ts';
+
 export type Protocol = {
   epoch: number;
   minFeeA: number;
@@ -20,3 +22,30 @@ export type Protocol = {
   maxCollateralInputs: number;
   coinsPerUTxOSize: string;
 };
+
+export const toProtocolParameters = (protocol: Protocol): ProtocolParamters => (<ProtocolParamters>{
+  txFeePerByte: protocol.minFeeA,
+  txFeeFixed: protocol.minFeeB,
+  maxBlockBodySize: protocol.maxBlockSize,
+  maxTxSize: protocol.maxTxSize,
+  maxBlockHeaderSize: protocol.maxBlockHeaderSize,
+  stakeAddressDeposit: BigInt(protocol.keyDeposit),
+  stakePoolDeposit: BigInt(protocol.poolDeposit),
+  minPoolCost: BigInt(protocol.minPoolCost),
+  utxoCostPerByte: BigInt(protocol.coinsPerUTxOSize),
+  executionUnitPrices: {
+    priceMemory: protocol.priceMem,
+    priceSteps: protocol.priceStep,
+  },
+  maxTxExecutionUnits: {
+    memory: parseInt(protocol.maxTxExMem),
+    steps: parseInt(protocol.maxTxExSteps),
+  },
+  maxBlockExecutionUnits: {
+    memory: parseInt(protocol.maxBlockExMem),
+    steps: parseInt(protocol.maxBlockExSteps),
+  },
+  maxValueSize: BigInt(protocol.maxValSize),
+  collateralPercentage: protocol.collateralPercent,
+  maxCollateralInputs: protocol.maxCollateralInputs,
+});
