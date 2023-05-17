565597645a58fcec17d2d5f828ed99f7eba3bf07
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2023-05-17 10:08:25 +0100

    implement toTxBuildOutput

--- packages/module/src/common/types/Recipient.ts
+++ packages/module/src/common/types/Recipient.ts
@@ -1,6 +1,9 @@
-import { Data } from './Data';
-import { NativeScript } from './NativeScript';
-import { PlutusScript } from './PlutusScript';
+import { 
+  Address, ITxBuildOutput, Hash32, hashData, txBuildOutToTxOut,
+} from '@harmoniclabs/plu-ts';
+import { Asset, toValue } from './Asset';
+import { Data, toPlutusData } from './Data';
+import { PlutusScript, toScript } from './PlutusScript';
 
 export type Recipient = string | {
   address: string;
@@ -8,5 +11,33 @@ export type Recipient = string | {
     value: Data;
     inline?: boolean;
   };
-  script?: PlutusScript | NativeScript;
+  script?: PlutusScript;
 };
+
+export const toTxBuildOutput = (recipient: Recipient | string, assets: Asset[]): ITxBuildOutput => {
+  if (typeof recipient === 'object') {
+    const plutusScriptReference = recipient.script
+      ? toScript(recipient.script)
+      : undefined;
+
+    const plutusData = recipient.datum
+      ? toPlutusData(recipient.datum.value)
+      : undefined;
+
+    const dataHash = plutusData
+      ? new Hash32(hashData(plutusData))
+      : undefined;
+
+    return txBuildOutToTxOut({
+      address: Address.fromString(recipient.address),
+      value: toValue(assets),
+      datum: recipient.datum?.inline ? plutusData : dataHash,
+      refScript: plutusScriptReference,
+    });
+  }
+
+  return txBuildOutToTxOut({
+    address: Address.fromString(recipient),
+    value: toValue(assets),
+  });
+}
